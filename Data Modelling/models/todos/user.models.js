7bef323b55434ec900b9c354cc2b42a0f0e25ee2
Krishna Kotgire <krishnakotgire12369@gmail.com>
2024-06-06 17:39:48 +0530

    User Data Model

--- Data Modelling/models/todos/user.models.js
+++ Data Modelling/models/todos/user.models.js
@@ -1,5 +1,9 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+  isActive: Boolean,
+});
 
 export const User = mongoose.model("User", userSchema);
