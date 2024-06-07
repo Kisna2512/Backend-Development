cacc2f519f49239335222162aa7825d2d852806c
Krishna Kotgire <krishnakotgire12369@gmail.com>
2024-06-07 12:57:08 +0530

    Todo Schema creation

--- Data Modelling/models/todos/user.models.js
+++ Data Modelling/models/todos/user.models.js
@@ -1,22 +1,25 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  username: {
-    typeof: String,
-    required: true,
-    unique: true,
-    lowercase: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      typeof: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    email: {
+      typeof: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      typeof: String,
+      required: true,
+    },
+    isActive: Boolean,
   },
-  email: {
-    typeof: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    typeof: String,
-    required: true,
-  },
-  isActive: Boolean,
-});
+  { timestamps: true }
+);
 
 export const User = mongoose.model("User", userSchema);
