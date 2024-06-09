beafa539eafd822e3a75a25c676cd1973ad2d8f6
Krishna Kotgire <krishnakotgire12369@gmail.com>
2024-06-09 16:27:59 +0530

    Category Schema is defined

--- Data Modelling/models/ecommerce/category.models.js
+++ Data Modelling/models/ecommerce/category.models.js
@@ -1,5 +1,13 @@
 import mongoose from "mongoose";
 
-const categorySchema = new mongoose.Schema({}, { timeStamps: true });
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  { timeStamps: true }
+);
 
 export const Category = mongoose.model("Category", categorySchema);
