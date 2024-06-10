11c0398b7b609142de894f12fd62ad6bbbf9a8f0
Krishna Kotgire <krishnakotgire12369@gmail.com>
2024-06-10 14:28:18 +0530

    Hospital model

--- Data Modelling/models/hospital-management/hospital.models.js
+++ Data Modelling/models/hospital-management/hospital.models.js
@@ -1,5 +1,24 @@
 import mongoose from "mongoose";
 
-const hospitalSchema = new mongoose.Schema({}, { timestamps: true });
+const hospitalSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    addredd: {
+      type: String,
+      required: true,
+    },
+
+    specializedIN: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 export const Hospital = mongoose.model("Hospital", hospitalSchema);
--- Data Modelling/models/hospital-management/medical_record.models.js
+++ Data Modelling/models/hospital-management/medical_record.models.js
@@ -1,6 +1,14 @@
 import mongoose from "mongoose";
 
-const medicalRecordSchema = new mongoose.Schema({}, { timestamps: true });
+const medicalRecordSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
 export const MedicalRecord = mongoose.model(
   "MedicalRecord",
