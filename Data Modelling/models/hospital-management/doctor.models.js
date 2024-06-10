6c160bccb8fb08ab3f9b749cfdf66d9bd0a1c367
Krishna Kotgire <krishnakotgire12369@gmail.com>
2024-06-10 14:23:13 +0530

    Doctor model

--- Data Modelling/models/hospital-management/doctor.models.js
+++ Data Modelling/models/hospital-management/doctor.models.js
@@ -1,5 +1,32 @@
 import mongoose from "mongoose";
 
-const doctorSchema = new mongoose.Schema({}, { timestamps: true });
+const doctorSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    salary: {
+      type: Number,
+      required: true,
+    },
+    qualifications: {
+      type: String,
+      required: true,
+    },
+    experienceInyears: {
+      type: Number,
+      required: true,
+      default: 1,
+    },
+    worksInhospital: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Hospital",
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 export const Doctor = mongoose.model("Doctor", doctorSchema);
