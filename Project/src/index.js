abfadc7bbbf130d0d2ee9f8bca3894b2c3567c2c
Krishna Kotgire <krishnakotgire12369@gmail.com>
2024-06-17 17:50:24 +0530

    MongoDB Succesfully connected

--- Project/src/index.js
+++ Project/src/index.js
@@ -1,20 +1,6 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
-import express from "express";
-const app = express();
-
-(async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    app.on("error", (error) => {
-      console.log("Error: ", error);
-      throw error;
-    });
-    app.listen(process.env.PORT, () => {
-      console.log(`App is running on port ${process.env.PORT}`);
-    });
-  } catch (error) {
-    console.error("Error: ", error);
-    throw error;
-  }
-})();
+import dotenv from "dotenv";
+import connectDB from "./db/connection.js";
+dotenv.config({
+  path: "./env",
+});
+connectDB();
